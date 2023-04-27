65baed4b1958564fe4036fda207b480ec020b065
Mayank Singh <31184015+mayank-singh-care@users.noreply.github.com>
2023-04-27 12:22:31 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -22,7 +22,7 @@ function Form(props) {
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
+          Hello, What needs to be done?
         </label>
       </h2>
 
